93bc2740ed5271702acb8c346ea77cf0cabcdfc8
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2022-08-30 16:15:42 +0000

    ESlint config

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -1,63 +1,67 @@
 <script setup>
-import { ref } from 'vue';
-import { Head, useForm } from '@inertiajs/inertia-vue3';
-import JetAuthenticationCard from '@/Components/AuthenticationCard.vue';
-import JetAuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import JetButton from '@/Components/Button.vue';
-import JetInput from '@/Components/Input.vue';
-import JetInputError from '@/Components/InputError.vue';
-import JetLabel from '@/Components/Label.vue';
+import { ref } from 'vue'
+import { Head, useForm } from '@inertiajs/inertia-vue3'
+import JetAuthenticationCard from '@/Components/AuthenticationCard.vue'
+import JetAuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue'
+import JetButton from '@/Components/Button.vue'
+import JetInput from '@/Components/Input.vue'
+import JetInputError from '@/Components/InputError.vue'
+import JetLabel from '@/Components/Label.vue'
 
 const form = useForm({
-    password: '',
-});
+  password: '',
+})
 
-const passwordInput = ref(null);
+const passwordInput = ref(null)
 
 const submit = () => {
-    form.post(route('password.confirm'), {
-        onFinish: () => {
-            form.reset();
+  form.post(route('password.confirm'), {
+    onFinish: () => {
+      form.reset()
 
-            passwordInput.value.focus();
-        },
-    });
-};
+      passwordInput.value.focus()
+    },
+  })
+}
 </script>
 
 <template>
-    <Head title="Secure Area" />
+  <Head title="Secure Area" />
 
-    <JetAuthenticationCard>
-        <template #logo>
-            <JetAuthenticationCardLogo />
-        </template>
+  <JetAuthenticationCard>
+    <template #logo>
+      <JetAuthenticationCardLogo />
+    </template>
 
-        <div class="mb-4 text-sm text-gray-600">
-            This is a secure area of the application. Please confirm your password before continuing.
-        </div>
+    <div class="mb-4 text-sm text-gray-600">
+      This is a secure area of the application. Please confirm your password before continuing.
+    </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <JetLabel for="password" value="Password" />
-                <JetInput
-                    id="password"
-                    ref="passwordInput"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                    autofocus
-                />
-                <JetInputError class="mt-2" :message="form.errors.password" />
-            </div>
+    <form @submit.prevent="submit">
+      <div>
+        <JetLabel for="password" value="Password" />
+        <JetInput
+          id="password"
+          ref="passwordInput"
+          v-model="form.password"
+          type="password"
+          class="mt-1 block w-full"
+          required
+          autocomplete="current-password"
+          autofocus
+        />
+        <JetInputError class="mt-2" :message="form.errors.password" />
+      </div>
 
-            <div class="flex justify-end mt-4">
-                <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Confirm
-                </JetButton>
-            </div>
-        </form>
-    </JetAuthenticationCard>
+      <div class="flex justify-end mt-4">
+        <JetButton
+          class="ml-4"
+          :class="{ 'opacity-25': form.processing }"
+          :disabled="form.processing"
+        >
+          Confirm
+        </JetButton>
+      </div>
+    </form>
+  </JetAuthenticationCard>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    ssr: {
-        noExternal: ['@inertiajs/server'],
-    },
-});
+  plugins: [
+    eslintPlugin(),
+    laravel({
+      input: 'resources/js/app.js',
+      ssr: 'resources/js/ssr.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  ssr: {
+    noExternal: ['@inertiajs/server'],
+  },
+})
