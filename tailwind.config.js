3bd2ac2b5a7688ee2bd65ecb80facda756c2406b
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2022-09-26 02:04:12 +0300

    Windows build

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+
   content: [
     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     './vendor/laravel/jetstream/**/*.blade.php',
@@ -11,6 +13,18 @@ module.exports = {
   ],
 
   theme: {
+    screens: {
+      sm: "576px",
+      "sm-max": { max: "576px" },
+      md: "768px",
+      "md-max": { max: "768px" },
+      lg: "992px",
+      "lg-max": { max: "992px" },
+      xl: "1200px",
+      "xl-max": { max: "1200px" },
+      "2xl": "1320px",
+      "2xl-max": { max: "1320px" },
+    },
     extend: {
       fontFamily: {
         sans: ['Arimo', ...defaultTheme.fontFamily.sans],
