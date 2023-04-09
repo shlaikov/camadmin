c0e21ca0fff3961c6fad7e67eed46fe67f002b62
Alexey Shlaikov <shlaikov.alexey@ya.ru>
2023-04-09 21:17:56 +0000

    Add some config; New way to create superuser

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Actions\Fortify\CreateNewUser;
+use Illuminate\Console\Command;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cmdmn:create_superuser';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creating initial superuser after instantiation';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(CreateNewUser $user)
+    {
+        if (User::all()->count() > 0) {
+            return Command::INVALID;
+        }
+
+        try {
+            $email = $this->ask('Enter your email');
+
+            $user->create([
+                'name' => 'Admin',
+                'email' => $email,
+                'password' => $this->secret('Enter password'),
+                'password_confirmation' => $this->secret(
+                    'Enter password conformation'
+                )
+            ]);
+
+            $this->info("Superuser [$email] successfully created");
+
+            return Command::SUCCESS;
+        } catch (\Throwable $e) {
+            $this->error($e->getMessage());
+
+            return Command::FAILURE;
+        }
+    }
+}
